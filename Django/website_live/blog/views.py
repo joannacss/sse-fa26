9603061d301c219658a3efc27053a7f1600b5c9d
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2026-09-21 15:14:31 -0400

    class demo

--- Django/website_live/blog/views.py
+++ Django/website_live/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    name = "SSE-FA26"
+    nickname = "JCSS"
+    return render(request, 'blog/index.html', context={'name': name, 'nickname': nickname})
+
+def register(request):
+    return render(request, 'blog/register.html')
