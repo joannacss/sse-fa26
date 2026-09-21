3879f7cb7658e195746d44cd1a9ac950943b7f86
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2026-09-21 13:51:02 -0400

    Checking forms

--- Django/website_live/blog/urls.py
+++ Django/website_live/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog'  # creates a namespace for this app
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     # path('login/', views.login, name='login'),
     # path('logout', views.logout, name='logout'),
     # path('post/create', views.create_post, name='create_post'),
--- Django/website_live/blog/views.py
+++ Django/website_live/blog/views.py
@@ -1,5 +1,25 @@
+from django import forms
 from django.shortcuts import render
+
+from blog.models import User
+
 
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    email = forms.EmailField()
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+def register(request):
+    form = RegistrationForm( request.POST or None)
+    context = {"form": form}
+    if request.method == 'POST':
+        # Handle registration logic here
+        pass
+    return render(request, 'blog/register.html', context)
