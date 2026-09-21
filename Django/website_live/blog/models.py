aef5ae3240d43ec433214194f77995ef92672665
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2026-09-21 14:43:13 -0400

    added code

---
+++ Django/website_live/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(unique=True,max_length=200)
+    password = models.CharField()
+    email = models.EmailField(unique=True)
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
