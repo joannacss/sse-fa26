0b50615a310c68ee6e5bea1665ad3250ae8feceb
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2026-09-23 11:29:34 -0400

    Added skeletons

--- Django/website_live/blog/views.py
+++ Django/website_live/blog/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     name = "SSE-FA26"
     nickname = "JCSS"
     return render(request, 'blog/index.html', context={'name': name, 'nickname': nickname})
 
+
 def register(request):
     return render(request, 'blog/register.html')
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def create_post(request):
+    pass
+
+
+def view_post(request, post_id):
+    pass
+
+
+def list_posts(request):
+    pass
